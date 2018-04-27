9239ddfe321068b41db67df972edb8b7dd374a5d
satoshi.torisu <satoccer@gmail.com>
2018-04-27 13:20:38 +0000

    init

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,14 +1,8 @@
 class AppsController < ApplicationController
   before_action :set_profile, only: [:index]
-
+  before_action :who_is_it, only: [:index]
   def index
-  	if user_signed_in?
-  		@admin = true if Member.where("user_id = ? and admin = ?",current_user.id, true).present?
-      @member = true if Member.where("user_id = ? and admin = ?",current_user.id, false).present?
-  		@brand_new = true unless Member.where("user_id = ? and admin = ?",current_user.id, false).present?
-      @invited = true if Member.where("user_id = ? and status = ?",current_user.id, 0).present?
-  	  @invited_by = Member.where("user_id = ? and status = ?",current_user.id, 0)
-    end
+
     # debugger
   end
 
@@ -20,4 +14,14 @@ class AppsController < ApplicationController
 		  	end
 	  	end
 	  end
+    def who_is_it
+      if user_signed_in?
+        @admin = true if Member.where("user_id = ? and admin = ?",current_user.id, true).present?
+        @member = true if Member.where("user_id = ? and admin = ?",current_user.id, false).present?
+        @brand_new = true unless Member.where("user_id = ? and admin = ?",current_user.id, false).present?
+        @invited = true if Member.where("user_id = ? and status = ?",current_user.id, 0).present?
+        @invited_by = Member.where("user_id = ? and status = ?",current_user.id, 0)
+        @games = Game.all
+      end
+    end
 end
